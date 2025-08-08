87c6901b6a3f3e771addd820f8d0865007168092
Anna <annamaria.auer0706@gmail.com>
2025-08-08 13:09:15 +0200

    First commit

---
+++ src/lib/button/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import "./button.scss";
+
+  export let label: string;
+  export let onClick: () => void;
+  export let variant: "primary" | "outlined" | "text" = "primary";
+</script>
+
+<button onclick={onClick} class={`button-${variant}`}>
+  {label}
+</button>
